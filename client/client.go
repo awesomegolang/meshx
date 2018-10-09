fd6048f48262440c080ddc6d95025392b2f38642
smallnest <smallnest@gmail.com>
2018-10-09 18:28:36 +0800

    implemented server/client agent of sidecar

--- client/client.go
+++ client/client.go
@@ -2,40 +2,129 @@ package client
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"strings"
+	"time"
 
 	"github.com/smallnest/meshx/client/adapter"
 	"github.com/smallnest/meshx/util"
 	"github.com/smallnest/rpcx/client"
+	"github.com/smallnest/rpcx/log"
+)
+
+const (
+	RPCAdaptor     = "rpc-adapter"
+	RPCXBasepath   = "rpcx-basepath"
+	RPCXRegistry   = "rpcx-registry"
+	RPCXFailmode   = "rpcx-failmode"
+	RPCXSelectmode = "rpcx-selectmode"
+
+	ServiceMethodName = "MeshXService.Invoke"
 )
 
 // Agent is the client agent (sidecar) in the client side.
 // It redirects requests to the meshx server agent.
 type Agent struct {
-	op         util.Option
-	adapter    adapter.Adapter
-	rpcxClient client.XClient
+	op          util.Option
+	adapter     adapter.Adapter
+	rpcxClients map[string]client.XClient
+
+	basePath       string
+	registry       string
+	rpcxFailmode   client.FailMode
+	rpcxSelectmode client.SelectMode
+	sd             client.ServiceDiscovery
 }
 
 // New creates an agent.
 func New(op util.Option) *Agent {
-	rpcName := op.Get("rpc-adapter")
+	rpcName := op.Get(RPCAdaptor)
 	ad := adapter.NewAdapter(rpcName, op)
+
+	rpcxBasepath := op.GetWithDefault(RPCXBasepath, "/rpcx")
+	rpcxRegistry := op.Get(RPCXRegistry)
+	rpcxFailmode := op.GetWithDefault(RPCXFailmode, "Failover")
+	rpcxSelectmode := op.GetWithDefault(RPCXSelectmode, "RoundRobin")
+
+	rpcxFailm, err := client.FailModeString(rpcxFailmode)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rpcxSelectm, err := client.SelectModeString(rpcxSelectmode)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	d, err := createServiceDiscovery(rpcxRegistry, rpcxBasepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return &Agent{
-		op:      op,
-		adapter: ad,
+		op:             op,
+		adapter:        ad,
+		rpcxClients:    make(map[string]client.XClient),
+		basePath:       rpcxBasepath,
+		registry:       rpcxRegistry,
+		rpcxFailmode:   rpcxFailm,
+		rpcxSelectmode: rpcxSelectm,
+		sd:             d,
 	}
 }
 
 // Start starts this agent.
 func (a *Agent) Start() error {
-	rpcxBasepath := a.op.GetWithDefault("rpcx-basepath", "/rpcx")
-	etcdAddr := a.op.Get("etcd-address")
-
-	d := client.NewEtcdDiscovery(rpcxBasepath, "MeshXService", []string{etcdAddr}, nil)
-	a.rpcxClient = client.NewXClient("MeshXService", client.Failover, client.RoundRobin, d, client.DefaultOption)
-
 	a.adapter.SetCallback(a)
 	return a.adapter.Start(a.op)
+}
+
+// Close closes all xclients.
+func (a *Agent) Close() error {
+	var err error
+	for k, v := range a.rpcxClients {
+		err = v.Close()
+		if err != nil {
+			log.Errorf("failed to close client for %s: %v", k, err)
+		}
+	}
+
+	return nil
+}
+
+func createServiceDiscovery(regAddr, basePath string) (client.ServiceDiscovery, error) {
+	i := strings.Index(regAddr, "://")
+	if i < 0 {
+		return nil, errors.New("wrong format registry address. The right fotmat is [registry_type://address]")
+	}
+
+	regType := regAddr[:i]
+	regAddr = regAddr[i+3:]
+
+	switch regType {
+	case "peer2peer": //peer2peer://127.0.0.1:8972
+		return client.NewPeer2PeerDiscovery("tcp@"+regAddr, ""), nil
+	case "multiple":
+		var pairs []*client.KVPair
+		pp := strings.Split(regAddr, ",")
+		for _, v := range pp {
+			pairs = append(pairs, &client.KVPair{Key: v})
+		}
+		return client.NewMultipleServersDiscovery(pairs), nil
+	case "zookeeper":
+		return client.NewZookeeperDiscoveryTemplate(basePath, []string{regAddr}, nil), nil
+	case "etcd":
+		return client.NewEtcdDiscoveryTemplate(basePath, []string{regAddr}, nil), nil
+	case "consul":
+		return client.NewConsulDiscoveryTemplate(basePath, []string{regAddr}, nil), nil
+	case "mdns":
+		client.NewMDNSDiscoveryTemplate(10*time.Second, 10*time.Second, "")
+	default:
+		return nil, fmt.Errorf("wrong registry type %s. only support peer2peer,multiple, zookeeper, etcd, consul and mdns", regType)
+	}
+
+	return nil, errors.New("wrong registry type. only support peer2peer,multiple, zookeeper, etcd, consul and mdns")
 }
 
 // Call sends a request.
@@ -47,9 +136,33 @@ func (a *Agent) Call(service string, method string, frame []byte, extra ...inter
 	}
 
 	reply := &Reply{}
-	err := a.rpcxClient.Call(context.Background(), "Mul", args, reply)
+	xc, err := a.getXClient(service)
+	if err != nil {
+		return nil, err
+	}
+	err = xc.Call(context.Background(), ServiceMethodName, args, reply)
 	if err != nil {
 		return nil, err
 	}
 	return reply.Data, nil
+}
+
+func (a *Agent) getXClient(service string) (xc client.XClient, err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			if ee, ok := e.(error); ok {
+				err = ee
+				return
+			}
+
+			err = fmt.Errorf("failed to get xclient: %v", e)
+		}
+	}()
+
+	if a.rpcxClients[service] == nil {
+		a.rpcxClients[service] = client.NewXClient(service, a.rpcxFailmode, a.rpcxSelectmode, a.sd.Clone(service), client.DefaultOption)
+	}
+	xc = a.rpcxClients[service]
+
+	return xc, err
 }
--- server/server.go
+++ server/server.go
@@ -1,11 +1,31 @@
 package server
 
 import (
+	"errors"
+	"fmt"
 	"log"
+	"strconv"
+	"strings"
+	"time"
 
+	metrics "github.com/rcrowley/go-metrics"
 	"github.com/smallnest/meshx/server/adapter"
 	"github.com/smallnest/meshx/util"
 	"github.com/smallnest/rpcx/server"
+	"github.com/smallnest/rpcx/serverplugin"
+)
+
+const (
+	RPCAdaptor   = "rpc-adapter"
+	RPCXBasepath = "rpcx-basepath"
+	RPCXRegistry = "rpcx-registry"
+	RPCXNetwork  = "rpcx-network" // agent服务的网络类型
+	RPCXAddress  = "rpcx-address" // agent服务的地址
+
+	ServiceName = "MeshXService"
+
+	RPCXMDNSDomain = "rpcx-mdns-domain"
+	RPCXMDNSPort   = "rpcx-mdns-port"
 )
 
 // Agent is the server agent (sidecar) in the server side.
@@ -18,7 +38,7 @@ type Agent struct {
 
 // New creates an agent.
 func New(op util.Option) *Agent {
-	rpcName := op.Get("rpc-adapter")
+	rpcName := op.Get(RPCAdaptor)
 	ad := adapter.NewAdapter(rpcName, op)
 	return &Agent{
 		op:      op,
@@ -28,12 +48,14 @@ func New(op util.Option) *Agent {
 
 // Start starts this agent.
 func (a *Agent) Start() error {
-	rpcxNetwork := a.op.GetWithDefault("rpcx-network", "tcp")
-	rpcxAddr := a.op.Get("rpcx-address")
+	rpcxNetwork := a.op.GetWithDefault(RPCXNetwork, "tcp")
+	rpcxAddr := a.op.Get(RPCXAddress)
+	rpcxRegAddr := a.op.Get(RPCXRegistry)
+	rpcxBasepath := a.op.Get(RPCXBasepath)
 
 	rpcxServer := server.NewServer()
-	addRegistryPlugin(rpcxServer, a.op)
-	rpcxServer.RegisterName("MeshXService", &MeshxService{agent: a}, "")
+	addRegistryPlugin(rpcxServer, rpcxRegAddr, rpcxBasepath, rpcxNetwork, rpcxAddr, a.op)
+	rpcxServer.RegisterName(ServiceName, &MeshxService{agent: a}, "")
 	a.rpcxServer = rpcxServer
 
 	go func() {
@@ -46,18 +68,75 @@ func (a *Agent) Start() error {
 	return a.adapter.Start(a.op)
 }
 
-func addRegistryPlugin(s *server.Server, op util.Option) {
+func addRegistryPlugin(s *server.Server, regAddr, basePath, rpcxNetwork, listenAddr string, op util.Option) error {
+	i := strings.Index(regAddr, "://")
+	if i < 0 {
+		return errors.New("wrong format registry address. The right fotmat is [registry_type://address]")
+	}
 
-	// r := &serverplugin.EtcdRegisterPlugin{
-	// 	ServiceAddress: "tcp@" + *addr,
-	// 	EtcdServers:    []string{*etcdAddr},
-	// 	BasePath:       *basePath,
-	// 	Metrics:        metrics.NewRegistry(),
-	// 	UpdateInterval: time.Minute,
-	// }
-	// err := r.Start()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// s.Plugins.Add(r)
+	regType := regAddr[:i]
+	regAddr = regAddr[i+3:]
+
+	switch regType {
+	case "peer2peer": //peer2peer://127.0.0.1:8972
+		return nil
+	case "multiple":
+		return nil
+	case "zookeeper":
+		r := &serverplugin.ZooKeeperRegisterPlugin{
+			ServiceAddress:   rpcxNetwork + "@" + listenAddr,
+			ZooKeeperServers: []string{regAddr},
+			BasePath:         basePath,
+			Metrics:          metrics.NewRegistry(),
+			UpdateInterval:   time.Minute,
+		}
+		err := r.Start()
+		if err != nil {
+			log.Fatal(err)
+		}
+		s.Plugins.Add(r)
+		return nil
+	case "etcd":
+		r := &serverplugin.EtcdRegisterPlugin{
+			ServiceAddress: rpcxNetwork + "@" + listenAddr,
+			EtcdServers:    []string{regAddr},
+			BasePath:       basePath,
+			Metrics:        metrics.NewRegistry(),
+			UpdateInterval: time.Minute,
+		}
+		err := r.Start()
+		if err != nil {
+			log.Fatal(err)
+		}
+		s.Plugins.Add(r)
+		return nil
+	case "consul":
+		r := &serverplugin.ConsulRegisterPlugin{
+			ServiceAddress: rpcxNetwork + "@" + listenAddr,
+			ConsulServers:  []string{regAddr},
+			BasePath:       basePath,
+			Metrics:        metrics.NewRegistry(),
+			UpdateInterval: time.Minute,
+		}
+		err := r.Start()
+		if err != nil {
+			log.Fatal(err)
+		}
+		s.Plugins.Add(r)
+		return nil
+	case "mdns":
+		domain := op.Get(RPCXMDNSDomain)
+		portStr := op.Get(RPCXMDNSPort)
+		port, err := strconv.Atoi(portStr)
+
+		r := serverplugin.NewMDNSRegisterPlugin(rpcxNetwork+"@"+regAddr, port, metrics.NewRegistry(), time.Minute, domain)
+		err = r.Start()
+		if err != nil {
+			log.Fatal(err)
+		}
+		s.Plugins.Add(r)
+		return nil
+	default:
+		return fmt.Errorf("wrong registry type %s. only support peer2peer,multiple, zookeeper, etcd, consul and mdns", regType)
+	}
 }
